c6fbeee2c6eae50d9ae25b23945ea3f4d625a351
Onkar Mudegol <omudegol@gmail.com>
2023-10-10 15:51:37 +0530

    Inheritance

---
+++ Inheritance/Inheritance_Ambiguity_Prob_Hyb_Virtual_Class.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class B{
+    protected:
+    int x;
+    public:
+    void get_dataB()
+    {
+        cout<<"Enter the value of x: ";
+        cin>>x;
+    }
+};
+class DB1 : public virtual B
+{
+    protected:
+    int y;
+    public:
+    void get_dataDB1()
+    {
+        cout<<"\nEnter the value of y: ";
+        cin>>y;
+    }
+};
+class DB2 : public virtual B
+{
+    protected:
+    int z;
+    public:
+    void get_dataDB2()
+    {
+        cout<<"\nEnter the value of z: ";
+        cin>>z;
+    }
+};
+class D : public DB1, public DB2{
+    public:
+    void sum()
+    {
+        int result;
+        result = x+y+z;
+        cout<<"\nResult is: "<<result;
+    }
+};
+int main()
+{
+    D obj1;
+    obj1.get_dataB();
+    obj1.get_dataDB1();
+    obj1.get_dataDB2();
+    obj1.sum();
+    return 0;
+}
